d0a4759004e508879249b036b04bd7e08e09453e
Webgoonie <webgoonie@gmail.com>
2024-01-20 19:58:22 -0500

    Resolved Avatar typescript src

--- client/src/components/Avatar.tsx
+++ client/src/components/Avatar.tsx
@@ -10,10 +10,10 @@ const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return ( 
     <Image 
       className="rounded-full" 
-      height={"30" }
-      width={"30" }
+      height={"30"}
+      width={"30"}
       alt="Avatar" 
-      src={src || '/images/placeholder.jpg'}
+      src={src || '/images/userPlaceholder.jpg'}
     />
    );
 }
--- client/src/components/Navbars/UserMenu.tsx
+++ client/src/components/Navbars/UserMenu.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import React from 'react'
+import { AiFillAudio, AiOutlineMenu, AiOutlineMenuFold } from 'react-icons/ai'
+import Avatar from '../Avatar'
 
 const UserMenu = () => {
   return (
     <div className='relative'>
-        <div>
+        <div className="flex flex-row items-center gap-3">
             <div
                 onClick={() => { console.log('clicked Used Menu')}}
                 className="
@@ -15,7 +17,19 @@ const UserMenu = () => {
             ">
                 <span>User Menu</span>
             </div>
-            
+            <div
+                onClick={() => { console.log('clicked subUser Menu')} }
+                className="
+                    p-4 md:py-1 md:px-2 
+                    border-[1px] border-neutral-200 flex flex-row
+                    items-center gap-3 rounded-full cursour-pointer
+                    hover:shadow-md transition
+                ">
+                <AiOutlineMenu />
+                <div className='hidden md:block'>
+                    <Avatar src={null} />
+                </div>
+            </div>
             
         </div>
     </div>
