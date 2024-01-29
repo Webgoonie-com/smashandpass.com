8fc81df4670a07e72b26be0a5ff93863a0d2f0f9
Webgoonie <webgoonie@gmail.com>
2024-01-28 21:51:46 -0500

    Edit Server Settings

--- client/src/components/Servers/ServerHeader.tsx
+++ client/src/components/Servers/ServerHeader.tsx
@@ -54,7 +54,7 @@ const ServerHeader = ({server, role}: ServerHeaderProps) => {
                             onClick={() => onOpen("editServer", {server})}
                             className="px-3 py-2 text-sm cursor-pointer"
                         >
-                            Server Settings
+                            Edit Server Settings
                             <Settings className="h-4 w-4 ml-auto" />
                         </DropdownMenuItem>
                     )}
