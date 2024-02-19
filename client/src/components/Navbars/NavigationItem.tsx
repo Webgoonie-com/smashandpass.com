0fc36dab3d9a760190241f16f66907848590daf4
Webgoonie <webgoonie@gmail.com>
2024-02-19 13:33:00 -0500

    Added EnvImageUrl

--- client/src/components/Navbars/NavigationItem.tsx
+++ client/src/components/Navbars/NavigationItem.tsx
@@ -24,7 +24,7 @@ export const NavigationItem  = ({
     
     const router = useRouter()
     
-    const envImageUrl = process.env.NEXT_PUBLIC_IMAGE_URL || "/images/"; 
+    const envImageUrl = process.env.NEXT_PUBLIC_IMAGE_URL || ""; 
 
 
     const onClick = () => {
@@ -59,7 +59,7 @@ export const NavigationItem  = ({
                         )}>
                         <Image
                             fill
-                            src={imageUrl}
+                            src={`${envImageUrl}`+imageUrl}
                             alt="User Channel Image"
                             sizes="48px"
                             className="relative"
