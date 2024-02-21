edff5782ffce5ff07846fae69bd1dd1d21b8c5d3
Webgoonie <webgoonie@gmail.com>
2024-02-20 23:12:08 -0500

    Configured SocketIndicator to Work Again

--- client/pages/api/socket/io.ts
+++ client/pages/api/socket/io.ts
@@ -10,13 +10,15 @@ export const config = {
   },
 };
 
-const socketUrl = process.env.NEXTAUTH_SECUREURL || "";
+const socketUrl = process.env.NEXTAUTH_URL || "";
 
 console.log('Hitting Io.ts', socketUrl)
 
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
   if (!res.socket.server.io) {
-    const path = `${socketUrl}/api/socket/io`;
+    console.log('Can Not Find Socket res.socket.server.io: ')
+    const path = `/api/socket/io`;
+    //const path = `${socketUrl}/api/socket/io`;
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
       path: path,
